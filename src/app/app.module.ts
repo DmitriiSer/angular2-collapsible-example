4b4ed4c15be52a436c05b51c42742415b3747877
Dmitrii Serikov <Dmitrii Serikov>
2017-04-13 22:14:55 -0400

    Added BrowserAnimationsModule to the app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -14,6 +15,7 @@ import { CollapsibleListModule } from './components/angular2-collapsible/index';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     CollapsibleListModule

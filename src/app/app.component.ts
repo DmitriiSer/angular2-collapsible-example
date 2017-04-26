8fd75c04e89ab31ce810875dbba1c9987acfb238
DmitriiSer <dmitrii.serikov@gmail.com>
2017-04-26 11:51:17 -0400

    Added more collapsible-table options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'This is an example application that shows how to use angular2-collapsible component';
+  title = 'angular2-collapsible Demo';
 
-  loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-  loremIpsumSmall = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+  loremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore` +
+  ` magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ` +
+  `Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ` +
+  `Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+  loremIpsumSmall = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, ` +
+  `sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`;
 
-  collapsibleType: string = 'accordion';
-  collapsibleTypeAccordion: boolean = true;
+  collapsibleType = 'accordion';
+  collapsibleTypeAccordion = true;
 
   activeTab = 1;
 
@@ -46,31 +50,51 @@ export class AppComponent {
     ]
   };
 
-  tableOptions = [
-    'bordered', 'borderedHorizontally', 'borderedVertically',
-    'striped',
-    'stripedOddColor', 'stripedEvenColor',
-    'highlight',
-    'highlightColor',
-    'activeColor',
-    // 'select', 'selectMultipleRows',
-    'centered',
-    'noTextSelect'
-  ];
-
   bordered = false;
   borderedHorizontally = false;
   borderedVertically = false;
   striped = false;
-  stripedOddColor = '#f2f2f2';
+  stripedOddColor = 'rgba(242,242,242,0.8)';
+  stripedOddTextColor = 'black';
   stripedEvenColor = 'transparent';
+  stripedEvenTextColor = 'black';
   highlight = false;
-  highlightColor = '#e2e2e2';
-  activeColor = '#d2d2d2';
+  highlightColor = 'rgba(222,222,222, 0.8)';
+  highlightTextColor = 'black';
+  activeColor = 'rgba(212,212,212, 0.8)';
+  activeTextColor = 'black';
+  centered = false;
   select = false;
   selectMultipleRows = false;
-  centered = false;
+  selectColor = 'rgba(212,212,212, 0.8)';
+  selectTextColor = 'black';
+  allowDeselectingRows = false;
+  allowKeyboardNavigation = true;
   noTextSelect = false;
+
+  tableOptions = [
+    { title: 'bordered', checked: this.bordered },
+    { title: 'borderedHorizontally', checked: this.borderedHorizontally },
+    { title: 'borderedVertically', checked: this.borderedVertically },
+    { title: 'striped', checked: this.striped },
+    { title: 'stripedOddColor' },
+    { title: 'stripedOddTextColor' },
+    { title: 'stripedEvenColor' },
+    { title: 'stripedEvenTextColor' },
+    { title: 'highlight', checked: this.highlight },
+    { title: 'highlightColor' },
+    { title: 'highlightTextColor' },
+    { title: 'activeColor' },
+    { title: 'activeTextColor' },
+    { title: 'centered', checked: this.centered },
+    { title: 'select', checked: this.select },
+    { title: 'selectMultipleRows', checked: this.selectMultipleRows },
+    { title: 'selectColor' },
+    { title: 'selectTextColor' },
+    { title: 'allowDeselectingRows', checked: this.allowDeselectingRows },
+    { title: 'allowKeyboardNavigation', checked: this.allowKeyboardNavigation },
+    { title: 'noTextSelect', checked: this.noTextSelect }
+  ];
 
   constructor() { }
 
@@ -83,42 +107,70 @@ export class AppComponent {
     }
   }
 
-  toggleOption(option: string, value: any): void {
+  typeOf(str: string): string {
+    if (str.toLowerCase().endsWith('color')) {
+      return 'string';
+    } else {
+      return 'boolean';
+    }
+  }
+
+  getOption(option: string): string {
+    switch (option) {
+      case 'stripedOddColor': return this.stripedOddColor;
+      case 'stripedOddTextColor': return this.stripedOddTextColor;
+      case 'stripedEvenColor': return this.stripedEvenColor;
+      case 'stripedEvenTextColor': return this.stripedEvenTextColor;
+      case 'highlightColor': return this.highlightColor;
+      case 'highlightTextColor': return this.highlightTextColor;
+      case 'activeColor': return this.activeColor;
+      case 'activeTextColor': return this.activeTextColor;
+      case 'selectColor': return this.selectColor;
+      case 'selectTextColor': return this.selectTextColor;
+      default:
+        return 'null';
+    }
+  }
+
+  setOption(option: string, value: any): void {
     switch (option) {
       case 'bordered': this.bordered = value; break;
       case 'borderedHorizontally': this.borderedHorizontally = value; break;
       case 'borderedVertically': this.borderedVertically = value; break;
       case 'striped': this.striped = value; break;
-      case 'stripedOddColor': this.stripedOddColor = value; break;
-      case 'stripedEvenColor': this.stripedEvenColor = value; break;
       case 'highlight': this.highlight = value; break;
-      case 'highlightColor': this.highlightColor = value; break;
-      case 'activeColor': this.activeColor = value; break;
       case 'select': this.select = value; break;
       case 'selectMultipleRows': this.selectMultipleRows = value; break;
       case 'centered': this.centered = value; break;
+      case 'allowDeselectingRows': this.allowDeselectingRows = value; break;
+      case 'allowKeyboardNavigation': this.allowKeyboardNavigation = value; break;
       case 'noTextSelect': this.noTextSelect = value; break;
-    }
-  }
-
-  typeOf(str: string): string {
-    switch (str) {
-      case 'stripedOddColor': return this.stripedOddColor;
-      case 'stripedEvenColor': return this.stripedEvenColor;
-      case 'highlightColor': return this.highlightColor;
-      case 'activeColor': return this.activeColor;
-      default:
-        return 'boolean';
+      case 'stripedOddColor': this.stripedOddColor = value; break;
+      case 'stripedOddTextColor': this.stripedOddTextColor = value; break;
+      case 'stripedEvenColor': this.stripedEvenColor = value; break;
+      case 'stripedEvenTextColor': this.stripedEvenTextColor = value; break;
+      case 'highlightColor': this.highlightColor = value; break;
+      case 'highlightTextColor': this.highlightTextColor = value; break;
+      case 'activeColor': this.activeColor = value; break;
+      case 'activeTextColor': this.activeTextColor = value; break;
+      case 'selectColor': this.selectColor = value; break;
+      case 'selectTextColor': this.selectTextColor = value; break;
     }
   }
 
   inputDisabled(option: string): boolean {
     switch (option) {
       case 'stripedOddColor':
+      case 'stripedOddTextColor':
       case 'stripedEvenColor':
+      case 'stripedEvenTextColor':
         return this.striped ? null : true;
       case 'highlightColor':
+      case 'highlightTextColor':
         return this.highlight ? null : true;
+      case 'selectColor':
+      case 'selectTextColor':
+        return this.select ? null : true;
     }
   }
 }
